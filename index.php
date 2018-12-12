e02fb08cfdca2aae6bf95f05a191b3fda0512dc9
vDaann <44371288+vDaann@users.noreply.github.com>
2018-12-12 13:45:23 +0100

    First Upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="shortcut icon" type="image/png" href="images/favi_dg.png"/>
+        <title>DG | Digital Creative Agency | Prototype-sessie</title>
+    </head>
+    <body>
+        <div id="container">
+
+            <div class="header-wrap">
+                <div class="header">
+                    <div class="logo">
+                        <img src="images/logo-dg.png">
+                        <p class="intro">Creative Digital Agency</p>
+                        <p class="outro">Full Service Bureau voor Digitale Groei</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="information-wrap">
+                <div class="information">
+                    <p class="bold">Eerst zien, dan geloven! Wij laten vooraf – vrijblijvend – zien wat we kunnen betekenen.</p>
+                    <p class="bold">In Prototype Sessie presenteren we onze visie en een werkend prototype van uw website of -applicatie.</p>
+
+                    <div class="left-info">
+                        <p class="bold">Waarom een Prototype Sessie?</p>
+                        <div class="separator"></div>
+                        <p>Het selecteren van een business partner voor de ontwikkeling en implementatie
+                            van uw Digitale Strategie is lastig. Zelfs met een zorgvuldig samengestelde
+                            shortlist en een helder geformuleerd ‘Request for Proposal’ blijkt er in de praktijk
+                            toch nog te vaak op onderbuikgevoel geselecteerd te worden.</p><br>
+                        <p>Bij DG denken we dat dit anders kan – en moet! Om die reden laten wij u
+                            graag <span class="bold">vooraf</span> zien wat we kunnen betekenen. In het DG|Creative Lab nodigen we u daarom
+                            graag uit voor een Prototype Sessie.</p>
+
+                    </div>
+
+                    <div class="right-info">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="form-wrap">
+                <div class="form">
+                    <form action="reservation.php" method="post">
+                        <div>
+                            <label>Voor- en achternaam</label><br>
+                            <input type="text" name="fullname"/>
+                        </div>
+                        <div>
+                            <label>Bedrijfsnaam</label><br>
+                            <input type="text" name="company"/>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
